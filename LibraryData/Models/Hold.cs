e38bc9bbc3232259c7413cc5ad44379484e07618
huseyinyildiz <huseyinyildiz005@gmail.com>
2021-02-21 19:44:13 +0300

    Step One

---
+++ LibraryData/Models/Hold.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Hold
+    {
+        public int Id { get; set; }
+
+        public virtual LibraryAsset LibraryAsset { get; set; }
+        public virtual LibraryCard LibraryCard { get; set; }
+
+        public DateTime HoldPlaced { get; set; }
+    }
+}
