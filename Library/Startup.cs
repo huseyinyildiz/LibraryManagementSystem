274b8f7fa0fd3a2ce5d052c95c7fc801fbe9fc34
huseyinyildiz <huseyinyildiz005@gmail.com>
2021-02-27 22:33:48 +0300

    Step 4

--- Library/Startup.cs
+++ Library/Startup.cs
@@ -30,6 +30,7 @@ namespace Library
             services.AddSingleton(Configuration);
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
             services.AddScoped<ICheckout, CheckoutService>();
+            services.AddScoped<IPatron, PatronService>();
 
             services.AddDbContext<LibraryContext>(options
                 => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
---
+++ LibraryData/IPatron.cs
@@ -0,0 +1,18 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    public interface IPatron
+    {
+        Patron Get(int id);
+        IEnumerable<Patron> GetAll();
+        void Add(Patron newPatron);
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId);
+        IEnumerable<Hold> GetHolds(int patronId);
+        IEnumerable<Checkout> GetCheckouts(int patronId);
+
+    }
+}
