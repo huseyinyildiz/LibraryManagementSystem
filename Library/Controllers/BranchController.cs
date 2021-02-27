e1782868b248d0de80b4c247cb642c7e8ccc0abd
huseyinyildiz <huseyinyildiz005@gmail.com>
2021-02-28 02:15:28 +0300

    Step 5

---
+++ Library/Controllers/BranchController.cs
@@ -0,0 +1,57 @@
+﻿using Library.Models.Branch;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+        public IActionResult Index()
+        {
+            var branches = _branch.GetAll().Select(branch=>new BranchDetailModel { 
+            
+                    Id=branch.Id,
+                    Name=branch.Name,
+                    IsOpen=_branch.IsBranchOpen(branch.Id),
+                    NumberOfAssets = _branch.GetAssets(branch.Id).Count(),
+                    NumberOfPatrons = _branch.GetPatrons(branch.Id).Count()
+
+            });
+
+            var model = new BranchIndexModel()
+            {
+                Branches = branches
+            };
+            return View(model);
+        }
+        public IActionResult Detail(int id)
+        {
+            var branch = _branch.Get(id);
+
+            var model = new BranchDetailModel
+            {
+                Id = branch.Id,
+                Name = branch.Name,
+                Address = branch.Address,
+                Telephone = branch.Telephone,
+                OpenDate = branch.OpenDate.ToString("yyyy-MM-dd"),
+                NumberOfAssets = _branch.GetAssets(id).Count(),
+                NumberOfPatrons = _branch.GetPatrons(id).Count(),
+                TotalAssetValue = _branch.GetAssets(id).Sum(a => a.Cost),
+                ImageUrl = branch.ImageUrl,
+                HoursOpen = _branch.GetBranchHours(id)
+
+            };
+            return View(model);
+        }
+    }
+}
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -31,6 +31,7 @@ namespace Library
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
             services.AddScoped<ICheckout, CheckoutService>();
             services.AddScoped<IPatron, PatronService>();
+            services.AddScoped<ILibraryBranch, LibraryBranchService>();
 
             services.AddDbContext<LibraryContext>(options
                 => options.UseSqlServer(Configuration.GetConnectionString("LibraryConnection")));
